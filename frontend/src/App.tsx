927187bf06113c71c56562e860603b501797727f
Muhammad Awais <awais.zafar@ocloudsolutions.net>
2025-01-21 18:29:44 +0500

    Tailwind Configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h4>Hello Docker</h4>
+    <h4 className='text-blue-400'>Hello Docker</h4>
   )
 }
 
