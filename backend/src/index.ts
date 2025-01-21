6f26202fde8e9e46ba83a024a11f6c618294f2a7
Muhammad Awais <awais.zafar@ocloudsolutions.net>
2025-01-21 18:23:17 +0500

    Updated BE Config handle graceful shutdown of server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,37 @@
 import express from 'express';
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    return res.status(200).json({message: "Server is running"})
+})
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+  return res.json({ message: 'Hello from Express!' });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// Graceful shutdown logic
+async function gracefulShutdown() {
+    console.log('\nServer Shutdown initiated...');
+    
+    // 1. Close HTTP server
+    server.close(async () => {
+      console.log('HTTP server closed');
+            
+      process.exit(0);
+    });
+  
+    // Force shutdown after 5 seconds
+    setTimeout(() => {
+      console.error('Forcing shutdown after timeout');
+      process.exit(1);
+    }, 5000);
+  }
+
+// Handle signals
+process.on('SIGINT', gracefulShutdown);  // CTRL+C
+process.on('SIGTERM', gracefulShutdown); // Docker stop
